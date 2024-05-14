faed701292368fa945bab60a4b9a11155b18a87a
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-05-14 14:26:38 +1000

    UserSchema email to be unique with index of 1

--- src/models/user/UserSchema.js
+++ src/models/user/UserSchema.js
@@ -16,12 +16,12 @@ const userSchema = new mongoose.Schema(
     },
     email: {
       type: String,
+      unique: true,
+      index: 1,
       required: true,
     },
     password: {
       type: String,
-      unique: true,
-      index: 1,
       required: true,
     },
   },
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -34,6 +34,11 @@ router.post("/", async (req, res, next) => {
           message: "Unable to create an account, try again later",
         });
   } catch (error) {
+    if (error.message.includes("E11000 duplicate key")) {
+      error.message =
+        "Another user already have this email, change your email and try again";
+      error.status = 200;
+    }
     next(error);
   }
 });
